a0cd2ba4f54255d4defbbfdd2ad993354fb0866b
link5209 <195140532@qq.com>
2018-11-25 20:49:15 +0800

    使用 Self，让compiler去智能分析

--- Sources/pop/main.swift
+++ Sources/pop/main.swift
@@ -1,11 +1,10 @@
 
-func binarySearch<T: Comparable>(sortedKeys: [T], forKey k: T) -> Int {
+func binarySearch(sortedKeys: [Int], forKey: Int) -> Int {
     var lo = 0
     var hi = sortedKeys.count
-    while (hi > lo) {
+    while hi > lo {
         let mid = lo + (hi - lo) / 2
-        // if sortedKeys[mid].precedes(k) {
-        if sortedKeys[mid] < k {
+        if sortedKeys[mid] < forKey {
             lo = mid + 1
         }
         else {
@@ -15,69 +14,22 @@ func binarySearch<T: Comparable>(sortedKeys: [T], forKey k: T) -> Int {
     return lo
 }
 
-// let arr = [3.0, 4.1, 5.0]
-let bs = binarySearch(sortedKeys: [2.1,3.4, 4.0], forKey: 4.0)
-print("res:", bs)
+// print("r0:", binarySearch(sortedKeys: [1,2,3], forKey: 3))
 
+// binarySearch(sortedKeys: [1.0,2.0,3.0], forKey: 3.0)
 
-//extension Collection where Element : Comparable, Index == Int {
-//
-//    public func binarySearch(forKey: Element) -> Int {
-//        var lo = 0
-//        var hi = self.count
-//        while (hi > lo) {
-//            let mid = lo + (hi - lo) / 2
-//            if self[mid] < forKey {
-//                lo = mid + 1
-//            }
-//            else {
-//                hi = mid
-//            }
-//        }
-//        return lo
-//    }
-//}
-//
-//let r2 = [2.1,3.4, 4.0].binarySearch(forKey: 3.4)
-//print("r2:", r2)
-//
-//
-//
-// class Number: Comparable {
-//     var value: Double = 0
-//
-//     init(_ value: Double) {
-//         self.value = value
-//     }
-//
-//     static func < (lhs: Number, rhs: Number) -> Bool {
-//         return lhs.value < rhs.value
-//     }
-//
-//     static func == (lhs: Number, rhs: Number) -> Bool {
-//         return lhs.value == rhs.value
-//     }
-// }
-//
-// let c1 = Number(1)
-// let c2 = Number(2)
-// let c3 = Number(3)
-//
-//let r3 = [c1,c2,c3].binarySearch(forKey: c3)
-//print("r3:", r3)
-
+// 使用 protocol，以此兼容各种实现了该protocol的对象都可以使用该函数，提高复用层次
 
 protocol Ordered {
-    func precedes(_ other: Self) -> Bool
+    func precedes(_ other: Ordered) -> Bool
 }
 
-func binarySearch<T: Ordered>(sortedKeys: [T], forKey k: T) -> Int {
+func binarySearchX(sortedKeys: [Ordered], forKey: Ordered) -> Int {
     var lo = 0
     var hi = sortedKeys.count
-    while (hi > lo) {
+    while hi > lo {
         let mid = lo + (hi - lo) / 2
-        if sortedKeys[mid].precedes(k) {
-//        if sortedKeys[mid] < k {
+        if sortedKeys[mid].precedes(forKey) {
             lo = mid + 1
         }
         else {
@@ -87,44 +39,20 @@ func binarySearch<T: Ordered>(sortedKeys: [T], forKey k: T) -> Int {
     return lo
 }
 
-class Number: Ordered {
-     var value: Double = 0
-
-     init(_ value: Double) {
-         self.value = value
-     }
-
-    func precedes(_ other: Number) -> Bool {
-        return value < other.value
-    }
- }
-
- let c1 = Number(1)
- let c2 = Number(2)
- let c3 = Number(3)
-
-let r1 = binarySearch(sortedKeys: [c1,c2,c3], forKey: c3)
-print("r1:", r1)
-
-
-extension Collection where Element : Ordered, Index == Int {
-    
-    func binarySearch(forKey k: Element) -> Int {
-        var lo = 0
-        var hi = self.count
-        while (hi > lo) {
-            let mid = lo + (hi - lo) / 2
-            if self[mid].precedes(k) {
-                lo = mid + 1
-            }
-            else {
-                hi = mid
-            }
-        }
-        return lo
+extension Int: Ordered {
+    func precedes(_ other: Ordered) -> Bool {
+        return self < (other as! Int).self
     }
 }
 
-let r4 = [c1,c2,c3].binarySearch(forKey: c2)
-print("r4:", r4)
+print("r0 Int:", binarySearchX(sortedKeys: [1,2,3], forKey: 3))
+
+
+extension Double: Ordered {
+    func precedes(_ other: Ordered) -> Bool {
+        return self < (other as! Double).self
+    }
+}
+
+print("r0 Double:", binarySearchX(sortedKeys: [1.0,2.0,3.0], forKey: 3.0))
 
