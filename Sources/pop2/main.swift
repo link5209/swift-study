cd04f7124f57439fee565725b552b589944f7cb8
link5209 <195140532@qq.com>
2018-12-01 22:51:08 +0800

    LinkedList using Sequence

--- Sources/pop2/main.swift
+++ Sources/pop2/main.swift
@@ -1,20 +1,83 @@
-     // struct Countdown: Sequence, IteratorProtocol {
-     //     var count: Int
+// Sequence
 
-     //     mutating func next() -> Int? {
-     //         if count == 0 {
-     //             return nil
-     //         } else {
-     //             defer { count -= 1 }
-     //             return count
-     //         }
-     //     }
-     // }
+struct Countdown: Sequence, IteratorProtocol {
+    var count: Int
 
-     // let threeToGo = Countdown(count: 3)
-     // for i in threeToGo {
-     //     print(i)
-     // }
+    mutating func next() -> Int? {
+        if count == 0 {
+            return nil
+        } else {
+            defer { count -= 1 }
+            return count
+        }
+    }
+}
+
+// let threeToGo = Countdown(count: 3)
+// for i in threeToGo {
+//     print(i)
+// }
+
+
+// LinkedList
+class Node {
+    let value: Any
+    var next: Node?
+
+    init(value: Any, next: Node?) {
+        self.value = value
+        self.next = next
+    }
+}
+
+class LinkedList: Sequence {
+    var head: Node?
+
+    func makeIterator() -> LinkedListIterator {
+        return LinkedListIterator(self)
+    }
+}
+
+struct LinkedListIterator: IteratorProtocol {
+    var current: Node?
+
+    init(_ linkedList: LinkedList) {
+        self.current = linkedList.head
+    }
+
+    mutating func next() -> Node? {
+        if let thisCurrent = current {
+            current = thisCurrent.next
+            return thisCurrent
+        }
+        else {
+            return nil
+        }
+    }
+}
+
+let node3 = Node(value: 3, next: nil)
+let node2 = Node(value: 2, next: node3)
+let node1 = Node(value: 1, next: node2)
+
+let linkedList = LinkedList()
+linkedList.head = node1
+
+for node in linkedList {
+    print(node.value)
+}
+
+
+// map,reduce,filter
+print(linkedList.map {($0.value as! Int) * 2})
+
+for node in (linkedList.filter {($0.value as! Int) < 3}) {
+    print(node.value)
+}
+
+
+
+
 
 
 
